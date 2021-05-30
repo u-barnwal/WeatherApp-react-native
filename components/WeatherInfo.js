68fce8134658f687301019d9a34bb9044f7cbb20
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-31 05:26:57 +0530

    show more weather data

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -1,22 +1,37 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export default function WeatherInfo({ currentWeather }) {
   if (!currentWeather) return null;
 
   const {
     main: { temp },
+    weather: [{ icon, description, main }],
+    name,
   } = currentWeather;
 
+  const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+
   return (
-    <View style={styles.weatherInfo}>
+    <View style={styles.info}>
+      <Text>{name}</Text>
+      <Image style={styles.icon} source={{ uri: iconUrl }} />
       <Text>{temp}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <Text>{main}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  weatherInfo: {
+  info: {
     alignItems: "center",
+  },
+  icon: {
+    width: 100,
+    height: 100,
+  },
+  description: {
+    textTransform: "capitalize",
   },
 });
