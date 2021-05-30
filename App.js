6a90762c047b80ff5f6c22e783e51ea1856cb4cd
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-31 04:42:40 +0530

    make API request and get tempreture

--- App.js
+++ App.js
@@ -3,8 +3,14 @@ import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import * as Location from "expo-location";
 
+const WEATHER_API_KEY = "b9907084d3a3897b632ee419f4d9519a";
+
+const getWeatherAPIUrl = (latitude, longitude) =>
+  `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`;
+
 export default function App() {
   const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
 
   useEffect(() => {
     load();
@@ -23,13 +29,37 @@ export default function App() {
 
       const { latitude, longitude } = location.coords;
 
-      alert("hello" + latitude, longitude);
-    } catch (error) {}
+      const response = await fetch(getWeatherAPIUrl(latitude, longitude));
+      const result = await response.json();
+
+      if (!response.ok) {
+        setErrorMessage(result.message);
+        return;
+      }
+
+      setCurrentWeather(result);
+    } catch (error) {
+      setErrorMessage(error);
+    }
   }
+
+  // + Showing error
+  if (errorMessage) {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  const {
+    main: { temp },
+  } = currentWeather;
 
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Hello {temp}</Text>
       <StatusBar style="auto" />
     </View>
   );
